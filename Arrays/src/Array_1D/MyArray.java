782e61f072322b826067f5583cd3cc30d089ce39
kencrimsonIT <tongkien930@gmail.com>
2026-07-21 21:17:07 +0700

    feat: Add getMissingValues() method

--- Arrays/src/Array_1D/MyArray.java
+++ Arrays/src/Array_1D/MyArray.java
@@ -56,8 +56,27 @@ public class MyArray {
      * Determine missing value from a sorted array
      * For example: [10 11 12 13 14 16 17 19 20] => 15 18
      */
-    public int[] getMissingValue() {
-        return null;
+    public int[] getMissingValues() {
+        int missingCount = 0; //Number of missing values
+        for (int i = 1; i < array.length; i++) {
+            int diff = array[i] - array[i - 1];
+            if (diff > 1) {
+                missingCount++;
+            }
+        }
+
+        int[] result = new int[missingCount]; //Missing values array
+        int index = 0;
+        for (int i = 1; i < array.length; i++) {
+            int diff = array[i] - array[i - 1];
+            if (diff > 1) {
+                for (int missingValue = array[i - 1] + 1; missingValue < array[i]; missingValue++) {
+                    result[index++] = missingValue;
+                }
+            }
+        }
+
+        return result;
     }
 
 
@@ -74,8 +93,8 @@ public class MyArray {
         int[] test1 = array1.mirror();
         int[] test2 = array2.removeDuplicates();
         int[] test3 = array3.removeDuplicates();
-        int[] test4 = array4.getMissingValue();
-        int[] test5 = array5.getMissingValue();
+        int[] test4 = array4.getMissingValues();
+        int[] test5 = array5.getMissingValues();
 
         System.out.println("Mirror: " + Arrays.toString(test1));
         System.out.println("Remove duplicate: " + Arrays.toString(test2));
